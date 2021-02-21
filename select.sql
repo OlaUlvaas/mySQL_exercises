a7869db7e8465d134c469c9e1dc9cab456ad1e67
Ola Ulvås <ola.ulvaas@gmail.com>
2021-02-21 22:40:25 +0100

    Update select.sql

--- select.sql
+++ select.sql
@@ -22,103 +22,139 @@ CREATE TABLE message(
 GREETING VARCHAR(255) not null,
 NUMBER INT not null
 );
--- insert into message (greeting, number) values("Hello World",123);
+INSERT INTO message (greeting, number) 
+VALUES("Hello World",123);
 
--- select * from message;
+/*SELECT * 
+FROM message;*/
 
 #
 # 2: Get everything from the city table
 # (Hint: Many many rows)
 
--- select * from city;
+/*SELECT * 
+FROM city;*/
 
 #
 # 3: Get everything on the cities whose district is "aceh"
 # (Hint: 2 rows)
 
--- select * from city where district = "aceh";
+/*SELECT * 
+FROM city 
+WHERE district = "aceh";*/
 
 #
 # 4: Get only the name of the cities where the countrycode is "bfa"
 #
 
--- select name from city where CountryCode = "bfa";
+/*SELECT name 
+FROM city 
+WHERE countrycode = "bfa";*/
 
 # 5: Get both the name and district of the cities where the countrycode is "tto"
 #
 
--- select name, district from city where CountryCode = "tto";
+/*SELECT name, district 
+FROM city 
+WHERE countrycode = "tto";*/
 
 
 # 6: Get the name and district named as nm,dist from the cities where the countrycode is "arm"
 
--- select name, district from city where CountryCode = "arm";
+/*SELECT name, district 
+FROM city 
+WHERE CountryCode = "arm";*/
 -- ????
 
 #
 # 7: Get the cities with a name that starts with "bor"
 
--- select * from city c where c.Name like 'bor%'
+/*SELECT * 
+FROM city c 
+WHERE c.Name like 'bor%'*/
 
 # 8: Get the cities with a name that contains the string "orto"
 
--- select * from city c where c.Name like '%orto%'
+/*SELECT * 
+FROM city c
+WHERE c.name like '%orto%'*/
 
 
 
 # 9: Get the cities that has a population below 1000
 
--- select * from city c where c.Population < 1000
+/*SELECT * 
+FROM city c 
+WHERE c.Population < 1000*/
 
 
 # 10: Get the unique countrycodes from the cities that has a population below 1000
 
 
--- select distinct CountryCode from city c where c.Population < 1000
+/*SELECT DISTINCT countrycode 
+FROM city c 
+WHERE c.Population < 1000*/
 
 
 
 
 # 11: Get the cities with the countrycode UKR that has more than 1000000 (one million) in population
 
--- select * from city c where c.Population > 1000000 && CountryCode = "UKR"
+/*SELECT * 
+FROM city c 
+WHERE c.population > 1000000 && countrycode = "UKR"*/
 
 
 # 12: Get the cities with a population of below 200 or above 9500000 (9.5 million)
 
 
--- select * from city where population < 200 or population > 9500000
+/*SELECT * 
+FROM city 
+WHERE population < 200 or population > 9500000*/
 
 
 # 13: Get the cities with the countrycodes TJK, MRT, AND, PNG, SJM
 
 
--- select * from city where CountryCode = "TJK" or CountryCode = "MRT" or CountryCode = "AND" or CountryCode = "PNG" or CountryCode = "SJM"
+/*SELECT * 
+FROM city 
+WHERE countrycode = "TJK" or CountryCode = "MRT" or CountryCode = "AND" or CountryCode = "PNG" or CountryCode = "SJM"*/
 
 
 # 14: Get the cities with a population between 200 and 700 inclusive
 
--- select * from city where Population >= 200 and population <= 700
+/*SELECT * 
+FROM city 
+WHERE Population >= 200 and population <= 700*/
 
 
 
 # 15: Get the countries with a population between 8000 and 20000 inclusive
 
--- select * from country where Population >= 8000 and Population <= 20000
+/*SELECT * 
+FROM country 
+WHERE Population >= 8000 and Population <= 20000*/
 
 
 # 16: Get the name of the countries with a independence year (indepyear) before year 0
 
--- select name from country where IndepYear < 0
+/*SELECT name 
+FROM country 
+WHERE IndepYear < 0*/
 
 
 # 17: Get the countries that has no recorded independence year and a population above 1000000
 
 
--- select * from country where Population > 1000000 and IndepYear is null
+/*SELECT * 
+FROM country 
+WHERE Population > 1000000 and IndepYear is null*/
 
 
 # 18: Get countries with a SurfaceArea below 10 and a defined LifeExpectancy
 
 
--- select * from country where SurfaceArea < 10 and LifeExpectancy is not NULL
+/*SELECT * 
+FROM country 
+WHERE SurfaceArea < 10 and LifeExpectancy is not NULL*/
+
